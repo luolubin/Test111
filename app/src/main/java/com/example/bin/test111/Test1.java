758404ad826cefa530732ad9aa542b89be8132ec
bin <llb9271@163.com>
2017-03-22 17:02:20 +0800

    新加入Text1文件

---
+++ app/src/main/java/com/example/bin/test111/Test1.java
@@ -0,0 +1,20 @@
+package com.example.bin.test111;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by bin on 2017/3/22.
+ */
+
+public class Test1 extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void printlnmy(){};
+
+}
